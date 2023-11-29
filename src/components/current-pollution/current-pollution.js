a5f3d366402d46d445efb5bee5a88505270e23db
Djordje Olujic <olujic.djordje@gmail.com>
2023-11-29 13:36:19 +0100

    Update current-pollution.js

--- src/components/current-pollution/current-pollution.js
+++ src/components/current-pollution/current-pollution.js
@@ -3,7 +3,7 @@ import GaugeChart from 'react-gauge-chart';
 
 
 const CurrentPollution = ({data}) => {
-    const color = data.list[0].main.aqi > 3 ? "red" : "green";
+    const color = data.list[0].main.aqi > 3 ? "#ef4444" : "#4ade80";
     const pollutionDesc = data.list[0].main.aqi > 3 ? "Poor" : "Good";
     const pollutionImg = data.list[0].main.aqi > 3 ? <img src="icons/smiley-with-mask.png" alt="bad" className='w-[100px]' /> : <img src="icons/happy-smiley.png" alt="happy" className='w-[80px]' />;
     const pollutionText = data.list[0].main.aqi > 3 ? <p>Air quality is poor and can be harmful for sensitive groups of people</p> : <p>Air quality is good and safe for people</p>
